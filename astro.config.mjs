b57d7421133fe9f31c2ed47262b7c170dddeed2e
Soheil Hosseini <soheil@immigratic.ca>
2025-08-21 15:21:18 -0400

    Install Vercel adapter and update dependencies - Add @astrojs/vercel package - Update package-lock.json with new dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,28 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://immigratic.ca',
+  output: 'static',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true
+    }
+  }),
   integrations: [tailwind()],
+  build: {
+    inlineStylesheets: 'auto'
+  },
+  vite: {
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: undefined
+        }
+      }
+    }
+  }
 });
